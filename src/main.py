650fd0f4022bf047f3e4854b8e103327aba7e9c7
iPegii <51372604+iPegii@users.noreply.github.com>
2024-02-03 14:14:06 +0200

    Initialize A-star algorithm

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import heapq
 import osmnx as ox
 
 
@@ -28,6 +29,51 @@ def process_map(graph):
 
 def draw_map(graph):
     ox.plot_graph(graph)
+
+def reconstruct_path(came_from, current):
+    total_path = current
+    while current in came_from.keys():
+        current = came_from[current]
+        total_path.append(current)
+    return total_path
+
+def a_star(start, goal, h):
+    """Parametrit:
+    start = aloituspiste, josta algoritmi aloittaa matkan
+    goal = päätöspiste, johon algoritmilla halutaan päästä
+    h = algoritmin heuristinen osuus, joka laskee jokaisen solmun hinnan 
+    """
+
+    #open_set sisältää tiedetyt solmut, joita voi laajentaa
+    open_set = [start]
+
+    came_from = []
+
+    currently_cheapest_to_n = []
+    currently_cheapest_to_n[start] = 0
+
+    forecasted_cheapest_path =[]
+    forecasted_cheapest_path[start] = h(start)
+
+    while len(open_set) > 0:
+        current = heapq.nsmallest(1, open_set, key=None)
+        if current == goal:
+            return reconstruct_path(came_from, current)
+
+        open_set.remove(current)
+        for neighbour in current:
+            weight_to_neighbour = 1 # but here weight between them
+         #   """Distance from start -> current -> neighbour"""
+            distance_to_neighbour = currently_cheapest_to_n[current] + weight_to_neighbour
+            if distance_to_neighbour < currently_cheapest_to_n[neighbour]:
+                came_from[neighbour] = current
+                currently_cheapest_to_n[neighbour] = forecasted_cheapest_path
+                forecasted_cheapest_path[neighbour] = forecasted_cheapest_path + h(neighbour)
+                if neighbour not in open_set:
+                    heapq.heappush(open_set, (neighbour))
+
+    return False
+
 
 
 main()
